fef9c90203b8db2fe4b503e435abf88556e01d9d
neil patel <neil@myindianweddingapp.com>
2014-10-02 20:00:30 +0100

    implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ Pinterestingv2::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { host: 'http://pinterestingv2.herokuapp.com' }
 end
